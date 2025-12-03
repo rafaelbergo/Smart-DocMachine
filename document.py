06f74a5c3573937c5ec3c6eb370aa468d62f260c
Rafael Bergo <73114569+rafaelbergo@users.noreply.github.com>
2025-12-03 10:59:26 -0300

    Update document.py

--- document.py
+++ document.py
@@ -9,10 +9,23 @@ DOCUMENT_PATH = os.path.join(DATA_DIR, 'document.jpg')
 
 def take_picture() -> bool:
     try:
-        result = subprocess.run(['libcamera-still', '-o', DOCUMENT_PATH], check=True)
-        print(f"[document] Foto salva em: {DOCUMENT_PATH}")
-        return result.returncode == 0
-    
+        print(f"[document] Capturando foto em: {DOCUMENT_PATH}")
+        # -n = sem preview
+        # -t 100 = tempo de exposiÃ§Ã£o de 100ms (ajustÃ¡vel)
+        result = subprocess.run(
+            ['libcamera-still', '-n', '-t', '100', '-o', DOCUMENT_PATH],
+            check=True,
+            capture_output=True,
+            text=True
+        )
+        print(f"[document] Foto salva com sucesso: {DOCUMENT_PATH}")
+        return True
+
+    except subprocess.CalledProcessError as e:
+        print(f"[document] Erro libcamera:\n{e.stderr}")
+
     except Exception as e:
         print(f"ERRO | def take_picture | {e}")
-        return False
+       
+    return False
+    
