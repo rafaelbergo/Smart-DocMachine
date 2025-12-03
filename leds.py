b5cc41d81c3339e0878e3d6ae85570ec718ecaa9
Rafael Bergo <73114569+rafaelbergo@users.noreply.github.com>
2025-12-03 10:59:51 -0300

    Update leds.py

--- leds.py
+++ leds.py
@@ -3,65 +3,87 @@ import RPi.GPIO as GPIO
 import time
 import os
 import threading
-from audio import playAudio, playAudio_async, start_recording, stop_recording
-from document import take_picture
+from audio import playAudio, start_recording, stop_recording, take_picture, text_to_audio, read_document, audio_to_text, ask_question, playAudio_async
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 AUDIOS_FOLDER = os.path.join(BASE_DIR, 'data', 'audios')
 DATA_DIR = os.path.join(BASE_DIR, 'data')
-AUDIO_PATH_QUESTION = os.path.join(DATA_DIR, 'audio_gravado.wav')  # arquivo de saída da gravação
+AUDIO_PATH_QUESTION = os.path.join(DATA_DIR, 'audio_gravado.wav')  # arquivo de saada da gravaao
+AUDIO_PATH_RESPONSE = os.path.join(DATA_DIR, 'resposta.wav')
 
-BUTTON_PHOTO_PIN = 16   # botão que dispara foto/fluxo LED + áudios
-BUTTON_REC_PIN   = 12   # NOVO: botão que grava enquanto estiver pressionado
+BUTTON_PHOTO_PIN = 16   # botao que dispara foto/fluxo LED + audios
+BUTTON_REC_PIN   = 12   # NOVO: botao que grava enquanto estiver pressionado
 LED_PIN = 26
 
 _initialized = False
-_busy = False  # evita reentrância da sequência de foto
+_busy = False  # evita reentrancia da sequancia de foto
 
-def _run_sequence():
+def _run_sequence(channel = None):
     """
     LED on -> 'processando' (async) -> 0.5s -> foto -> 5s -> LED off -> 'doc_processado' (sync)
     """
-    global _busy
     try:
-        playAudio_async(os.path.join(AUDIOS_FOLDER, 'processando_doc.wav'))
-        time.sleep(0.5)
+        #playAudio_async(os.path.join(AUDIOS_FOLDER, 'processando_doc.wav'))
+        playAudio_async(os.path.join(AUDIOS_FOLDER, 'processando_documento.wav'))
+        
+        GPIO.output(LED_PIN, GPIO.HIGH)
 
         ok = take_picture()
         print("[leds] Foto capturada." if ok else "[leds] Falha ao tirar foto.")
 
-        time.sleep(5)
+        
         GPIO.output(LED_PIN, GPIO.LOW)
-        print("[leds] LED desligado após captura.")
-        playAudio(os.path.join(AUDIOS_FOLDER, 'doc_processado.wav'))
-    finally:
-        _busy = False
+        print("[leds] LED desligado apas captura.")
+        
+        if not read_document():
+            print('erro read_document')
+            return
+        
+        playAudio(os.path.join(AUDIOS_FOLDER, 'documento_processado_sucesso.wav'))
+    except: 
+        print(f'ERRO | def _run_sequence | {str(e)}')
+
 
 def _toggle_photo_sequence(channel):
     global _busy
     if _busy:
-        print("[leds] Sequência em andamento; ignorando novo toque.")
+        print("[leds] Sequancia em andamento; ignorando novo toque.")
         return
     _busy = True
     GPIO.output(LED_PIN, GPIO.HIGH)
-    print("[leds] LED ligado! Iniciando sequência de foto.")
+    print("[leds] LED ligado! Iniciando sequancia de foto.")
     threading.Thread(target=_run_sequence, daemon=True).start()
+    
 
 def _record_edge(channel):
     """
-    Callback para o botão de gravação (BCM 12).
-    FALLING (pressionou)  -> inicia gravação
-    RISING  (soltou)      -> para gravação
+    Callback para o botao de gravaao (BCM 12).
+    FALLING (pressionou)  -> inicia gravaao
+    RISING  (soltou)      -> para gravaao
     """
     level = GPIO.input(BUTTON_REC_PIN)
     if level == GPIO.LOW:
         # pressionado
-        print("[leds] botão REC pressionado -> start_recording")
+        print("[leds] botao REC pressionado -> start_recording")
         start_recording(AUDIO_PATH_QUESTION, sample_rate=44100, channels=1)
     else:
         # solto
-        print("[leds] botão REC solto -> stop_recording")
+        print("[leds] botao REC solto -> stop_recording")
         stop_recording()
+        text = audio_to_text(AUDIO_PATH_QUESTION)
+        if not text:
+            return
+        
+        responsta_ia = ask_question(text)
+        #responsta_ia = 'eu sou sofia, uma assistente de inteligencia aritificial do projeto smart-doc-machine. fui criada por alunos de engenharia da computacao para ajudar pessoas a entenderem documentos.'
+
+        if not responsta_ia:
+            return
+                
+        text_to_audio(responsta_ia, AUDIO_PATH_RESPONSE)
+        
+        playAudio(AUDIO_PATH_RESPONSE)
+        
 
 def setup():
     global _initialized
@@ -70,12 +92,12 @@ def setup():
 
     GPIO.setmode(GPIO.BCM)
 
-    # botão da foto
+    # botao da foto
     GPIO.setup(BUTTON_PHOTO_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(BUTTON_PHOTO_PIN, GPIO.FALLING,
-                          callback=_toggle_photo_sequence, bouncetime=300)
+                          callback=_run_sequence, bouncetime=300)
 
-    # botão de gravação (segurar)
+    # botao de gravaao (segurar)
     GPIO.setup(BUTTON_REC_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(BUTTON_REC_PIN, GPIO.BOTH,   # ambas as bordas
                           callback=_record_edge, bouncetime=50)
@@ -95,7 +117,7 @@ def cleanup():
         GPIO.remove_event_detect(BUTTON_REC_PIN)
     except Exception:
         pass
-    # garante parar gravação se ainda ativa
+    # garante parar gravaao se ainda ativa
     try:
         stop_recording()
     except Exception:
